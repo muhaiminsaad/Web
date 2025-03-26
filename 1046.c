2404bf6fb56c3ba92dbd5a6e394eb9da9b024c18
muhaiminsaad <201452209+muhaiminsaad@users.noreply.github.com>
2025-03-26 21:43:02 +0600

    Update 1046.c

--- 1046.c
+++ 1046.c
@@ -1,20 +1,17 @@
-Read the start time and end time of a game, in hours. Then calculate the duration of the game, knowing that the game can begin in a day and finish in another day, with a maximum duration of 24 hours. The message must be printed in portuguese “O JOGO DUROU X HORA(S)” that means “THE GAME LASTED X HOUR(S)”
+#include <stdio.h>
 
-Input
-Two integer numbers representing the start and end time of a game.
+int main() {
+    int start, end, duration;
 
-Output
-Print the duration of the game as in the sample output.
+    scanf("%d %d", &start, &end);
 
-Input Sample	Output Sample
-16 2
+    if (start < end) {
+        duration = end - start;
+    } else {
+        duration = (24 - start) + end;
+    }
 
-O JOGO DUROU 10 HORA(S)
-
-0 0
-
-O JOGO DUROU 24 HORA(S)
-
-2 16
-
-O JOGO DUROU 14 HORA(S)
+    printf("O JOGO DUROU %d HORA(S)\n", duration);
+    
+    return 0;
+}
