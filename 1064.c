20fb00cf1bc8e506cec3ec70246db694e347f871
muhaiminsaad <201452209+muhaiminsaad@users.noreply.github.com>
2025-03-27 13:01:06 +0600

    Create 1064.c

---
+++ 1064.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    float num, sum = 0;
+    int count = 0;
+    
+
+    for (int i = 0; i < 6; i++) {
+        scanf("%f", &num);
+        
+        if (num > 0) {
+            sum += num;
+            count++;
+        }
+    }
+
+    printf("%d valores positivos\n", count);
+    
+    if (count > 0) {
+        printf("%.1f\n", sum / count);
+    }
+    
+    return 0;
+}
