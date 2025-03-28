fabbdde160d485523729c69c367e28e622f01584
muhaiminsaad <201452209+muhaiminsaad@users.noreply.github.com>
2025-03-28 21:55:34 +0600

    Update 1098.c

--- 1098.c
+++ 1098.c
@@ -4,13 +4,12 @@ int main() {
     int i, j;
     float I, J;
 
-    for (i = 0; i <= 10; i++) { // Using integer loop to avoid floating point precision issues
-        I = i / 5.0; // Convert integer to float (increments of 0.2)
+    for (i = 0; i <= 10; i++) {
+        I = i / 5.0;
 
         for (j = 1; j <= 3; j++) {
             J = j + I;
 
-            // Print as integer if I and J are whole numbers
             if (i % 5 == 0) { 
                 printf("I=%d J=%d\n", (int)I, (int)J);
             } else {
