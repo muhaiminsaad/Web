6503af91958bddff9f3944f83ccd0f3d3e67f2f2
muhaiminsaad <201452209+muhaiminsaad@users.noreply.github.com>
2025-03-28 21:37:50 +0600

    Create 1097.c

---
+++ 1097.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int I, J;
+
+    for (I = 1; I <= 9; I += 2) {
+
+        for (J = I + 6; J >= I + 4; J--) {
+            printf("I=%d J=%d\n", I, J);
+        }
+    }
+
+    return 0;
+}
